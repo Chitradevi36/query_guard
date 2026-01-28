a00b816a64bf0eff8ecf326a631001f657cf6047
Chitradevi36 <chitra.rajaguru123@gmail.com>
2026-01-28 03:39:39 +0000

    Integration

--- lib/query_guard.rb
+++ lib/query_guard.rb
@@ -3,21 +3,23 @@ require "active_support"
 require "active_support/notifications"
 require "query_guard/version"
 require "query_guard/config"
+require "query_guard/store"
+require "query_guard/security"
 require "query_guard/subscriber"
+require "query_guard/action_controller_subscriber"
 require "query_guard/middleware"
 require "query_guard/client"
 
 module QueryGuard
   class << self
     attr_accessor :client
-    # Keep config as a normal module ivar; no mattr_*
+
     def config
       @config ||= Config.new
     end
 
     def configure
       yield(config)
-      # Build a reusable HTTP client (whatever your Client class is)
       @client = Client.new(
         base_url: config.base_url,
         api_key:  config.api_key,
@@ -28,13 +30,10 @@ module QueryGuard
     end
 
     def install!(app = nil)
-      # Ensure config exists even if user didn't call configure
       config
-
-      # Install SQL subscriber once
       Subscriber.install!(config)
+      ActionControllerSubscriber.install!(config)
 
-      # Insert middleware
       if defined?(Rails) && Rails.respond_to?(:application) && Rails.application
         Rails.application.config.middleware.use(QueryGuard::Middleware, config)
       elsif app
@@ -46,7 +45,6 @@ module QueryGuard
   end
 end
 
-# Auto-install for Rails via Railtie
 if defined?(Rails::Railtie)
   module QueryGuard
     class Railtie < Rails::Railtie
